8ed34a0a9222973f5dab076f75f2a6c74ab6e149
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-04-01 18:49:12 -0700

    added drop table cascading for tests

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS github_users;
+DROP TABLE IF EXISTS github_users CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
 
 CREATE TABLE github_users (
   username TEXT NOT NULL PRIMARY KEY,
